50facfdf28f424c4007660187f18fa4088186771
Jônatas Mendes da Cruz <62781857+Kyoto-01@users.noreply.github.com>
2020-03-29 18:12:30 -0300

    Add files via upload

---
+++ Labirinto/SistemaGeral.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Labirinto
+{
+    class SistemaGeral
+    {
+        public void setVenceu(int personagemX, int personagemY)
+        {
+            if (personagemX == 19 && personagemY == 7)
+            {
+                fazerSeVencer();
+            }
+        }
+
+        private void fazerSeVencer()
+        {
+            Console.Clear();
+            Console.WriteLine("PARABÉNS, VOCÊ VENCEU!\n");
+            menuFinal();
+        }
+
+        private void menuFinal()
+        {
+            byte opcao = 0;
+
+            Console.WriteLine("1 p/ Jogar novamente");
+            Console.WriteLine("2 p/ Sair\n");
+
+            Console.Write("O que deseja fazer? ");
+            byte.TryParse(Console.ReadLine(), out opcao);
+
+            if (opcao == 1)
+            {
+                jogarNovamente();
+            }
+            else if (opcao == 2)
+            {
+                Environment.Exit(0);
+            }
+            fazerSeVencer();
+        }
+
+        private void jogarNovamente()
+        {
+            Console.Clear();
+            Program.Main();
+            return;
+        }
+    }
+}
