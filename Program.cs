8e7d1e4e297707d070c0e246ef809551f2210936
Jônatas Mendes da Cruz <62781857+Kyoto-01@users.noreply.github.com>
2020-03-29 17:03:11 -0300

    Labirinto de caracteres - archives
    Mais arquivos do jogo "Labirinto de caracteres"

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Labirinto
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.Title = "Labirinto";
+
+            personagem personagem1 = new personagem();
+            cenarioJogo cenario = new cenarioJogo();
+            SistemaGeral sg = new SistemaGeral();
+
+            cenario.atualizarTela();
+            while (true)
+            {
+                personagem1.controlarPersonagem();
+                sg.setVenceu(personagem1.LocalizacaoXPersonagem, personagem1.LocalizacaoYPersonagem);
+            }
+        }
+    }
+}
